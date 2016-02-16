929045389366100f6bb4a02fa10aeb099221af84
福田かるな <ring.ring.0114@gmail.com>
2016-02-16 12:32:35 +0900

    Initial Commit

---
+++ mudachecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mudachecker
+//
+//  Created by 福田かるな on 2016/02/16.
+//  Copyright © 2016年 net.karuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
